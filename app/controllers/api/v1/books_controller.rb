d2b788cdbb4c4d787b6286bf364319edf304dc46
jennwah <olovepro@gmail.com>
2021-01-02 01:18:55 +0800

    Added create POST route for creating book

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,5 +3,21 @@ class Api::V1::BooksController < Api::V1::ApiController
         @books = Book.all
         render json: @books
     end
+
+    def create
+        @book = Book.new(book_params)
+
+        if @book.save
+            json_string = {'message' => 'Book is successfully Created!!'}.to_json
+            format.json { render :json => {item: @book, json_string}
+        else
+            format.json { render json: @book.errors, status: :unprocessable_entity }
+        end
+    end
+
+    private
+        def book_params
+            params.require(:book).permit(:title, :description)
+        end
 end
 
